8307b8e169236d0cd020eab6033bf14797656e86
Ahmed Mohsen <163747371+amohsen98@users.noreply.github.com>
2024-10-20 15:01:49 +0300

    Shopping Cart Ui and Total Proce

--- E-Commerce/Areas/Customer/Controllers/CartController.cs
+++ E-Commerce/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_commerce.Entities.Repositories;
+using E_commerce.Entities.ViewModels;
+using E_Commerce.Entites.Models;
+using E_Commerce.Entities.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace E_Commerce.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
+
+
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; } //instance of shoppingcartVM
+
+        public CartController(IUnitOfWork unitOfWork )
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            ShoppingCartVM = new ShoppingCartVM()
+            {
+                //Get list of carts of corrent user who is logged right now
+                 CartsList = _unitOfWork.ShoppingCart.GetAll(u=>u.ApplicationUserId == claim.Value , Includeword : "Product")
+
+            };
+            foreach (var item in ShoppingCartVM.CartsList)
+            {
+				ShoppingCartVM.TotalCarts += (item.Count * int.Parse(item.Product.Price));
+
+
+            }
+
+            return View(ShoppingCartVM );
         }
-    }
+
+		//The plus count affect database (shoppingcart table)
+		public IActionResult Plus(int cartid)
+		{
+			var shoppingcart = _unitOfWork.ShoppingCart.GetFirstorDefault(x => x.Id == cartid);
+			_unitOfWork.ShoppingCart.IncreaseCount(shoppingcart, 1);
+			_unitOfWork.Complete();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Minus(int cartid)
+		{
+			var shoppingcart = _unitOfWork.ShoppingCart.GetFirstorDefault(x => x.Id == cartid);
+
+			if (shoppingcart.Count <= 1)
+			{
+				_unitOfWork.ShoppingCart.Remove(shoppingcart);
+				_unitOfWork.Complete();
+				return RedirectToAction("Index", "Home");
+
+
+			}
+			else
+			{
+				_unitOfWork.ShoppingCart.DecreaseCount(shoppingcart, 1);
+
+			}
+			_unitOfWork.Complete();
+			return RedirectToAction("Index");
+		}
+		public IActionResult Remove(int cartid) {
+
+
+			var shoppingcart = _unitOfWork.ShoppingCart.GetFirstorDefault(x => x.Id == cartid);
+			_unitOfWork.ShoppingCart.Remove(shoppingcart);
+			_unitOfWork.Complete();
+			return RedirectToAction("Index");
+		}
+
+	}
 }
---
+++ E_commerce.Entities/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Entites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Entities.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> CartsList { get; set; }
+        public int TotalCarts { get; set; }
+    }
+}
